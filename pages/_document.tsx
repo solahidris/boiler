894ea4c259676b300dd8691b4cb758ee7997ed60
solahidris <emailsolah@gmail.com>
2024-05-06 20:38:26 +0800

    SEO

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* <meta name="description" content="IMPORTANT! WRITE WEBSITE DESCRIPTION HERE! --- bla bla bla this is a copy" /> */}
+        {/* <meta name="keywords" content="IMPORTANT! WEBSITE KEYWORDS! --- sample, keyword, for, website" /> */}
+        <meta name="author" content="Solah Idris" />
+        {/* <title>WebsiteNameHere</title> */}
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Sacramento&display=swap" rel="stylesheet" />
