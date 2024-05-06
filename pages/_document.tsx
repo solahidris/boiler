e00c5073e0f6a9caa20f6a85e48e06164df0ece6
solahidris <emailsolah@gmail.com>
2024-05-07 00:27:27 +0800

    opengraph seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,25 +5,31 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Standard meta tags */}
+        {/* <!-- Open Graph Meta Tags --> */}
+        {/* <title>HireSolah</title> */}
+        <meta property="og:locale" content="en_US" />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-        {/* Open Graph meta tags */}
-        <meta name="author" content="Solah Idris" />
-        <meta property="og:type" content="website" />
-        <meta property="og:locale" content="en_US" />
         {/* <meta name="description" content="HireSolah - Websites built by experts just for you. Save cost by communicating directly with the developers to solve your needs. Multiple flexible monthly payment options. Get your stunning website up and running in no time!" /> */}
         {/* <meta name="keywords" content="website, development, experts, monthly payment, flexible, communication, developers" /> */}
+        <meta name="author" content="Solah Idris" />
+
+        {/* <!-- Facebook Meta Tags --> */}
         {/* <meta property="og:url" content="https://www.hiresolah.com/" /> */}
-        {/* <meta property="og:image" content="/homepage_image.png" /> */}
-        
-        {/* Twitter Card meta tags (optional) */}
-        {/* <meta name="twitter:card" content="https://pbs.twimg.com/profile_images/1757732310315900928/XUjDh9vB_400x400.jpg" /> */}
+        <meta property="og:type" content="website" />
+        {/* <meta property="og:title" content="HireSolah" /> */}
+        {/* <meta property="og:description" content="HireSolah - Websites built by experts just for you. Save cost by communicating directly with the developers to solve your needs. Multiple flexible monthly payment options. Get your stunning website up and running in no time!" /> */}
+        {/* <meta property="og:image" content="https://hiresolah.com/fb_og_image_1200_630.png" /> */}
+
+        {/* <!-- Twitter Meta Tags --> */}
+        {/* <meta name="twitter:card" content="summary_large_image" /> */}
         {/* <meta name="twitter:site" content="@solaheth_" /> */}
-        {/* <meta name="twitter:title" content="HireSolah" /> */}
-        {/* <meta name="twitter:description" content="Save cost by communicating directly with the developers to create your prime website." /> */}
-        {/* <meta name="twitter:image" content="https://pbs.twimg.com/profile_images/1757732310315900928/XUjDh9vB_400x400.jpg" /> */}
+        {/* <meta name="twitter:creator" content="@solaheth_" /> */}
+        {/* <meta name="twitter:title" content="HireSolah for building websites" /> */}
+        {/* <meta name="twitter:description" content="HireSolah - Websites built by experts just for you. Save cost by communicating directly with the developers to solve your needs. Multiple flexible monthly payment options. Get your stunning website up and running in no time!" /> */}
+        {/* <meta name="twitter:image" content="https://hiresolah.com/twitter_image_1200_630.png" /> */}
+        {/* <meta property="twitter:domain" content="hiresolah.com" /> */}
+        {/* <meta property="twitter:url" content="https://www.hiresolah.com/" /> */}
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
