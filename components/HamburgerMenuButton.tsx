fe2832e6860a823599cca06c0f7c7e65cd648669
Sol <emailsolah@gmail.com>
2024-09-19 14:08:39 +0800

    Update HamburgerMenuButton.tsx

--- components/HamburgerMenuButton.tsx
+++ components/HamburgerMenuButton.tsx
@@ -7,7 +7,7 @@ const HamburgerMenuButton = () => {
   const [showMenu, setShowMenu] = useState(false);
   const mutedBackground = "bg-accent-content/50 w-[100vw] h-[100vh] fixed z-8 top-0 left-0"
   const navigationContainerCSS = "fixed z-10 h-[100vh] top-0 right-0 bg-base-100 transition-all ease-in-out duration-300 shadow-lg"
-  const navigationLinkCSS = "flex flex-col gap-4 w-full h-full font-bold justify-end"
+  const navigationLinkCSS = "flex flex-col gap-8 w-full h-full font-bold justify-end"
   
   return (
     <div>
@@ -16,7 +16,7 @@ const HamburgerMenuButton = () => {
       </div>
       <div className={`${showMenu && mutedBackground}`}>
         {showMenu ?
-            <div className={`w-[85vw] lg:w-[75vw] opacity-100 p-6 lg:p-20 ${navigationContainerCSS}`} onMouseLeave={()=>setShowMenu(false)}>
+            <div className={`w-[85vw] lg:w-[75vw] opacity-100 p-12 lg:p-20 ${navigationContainerCSS}`} onMouseLeave={()=>setShowMenu(false)}>
               <div className={`${navigationLinkCSS}`}>
                 <WhileInViewAnimation classNameProp={"text-end"}><NavigationLink linkName={"Home"} linkPath={"/"} setShowMenu={setShowMenu} /></WhileInViewAnimation>
                 <WhileInViewAnimation classNameProp={"text-end"}><NavigationLink linkName={"About"} linkPath={"/about"} setShowMenu={setShowMenu} /></WhileInViewAnimation>
