9e5f0421c5b2feec9d1e30870cb50c78a0bef078
Sol <emailsolah@gmail.com>
2024-10-02 01:32:27 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-// import { Analytics } from '@vercel/analytics/react';
 
 export default function Document() {
   return (
@@ -52,7 +51,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        {/* <Analytics /> */}
       </body>
     </Html>
   );
