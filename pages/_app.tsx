31227afba6c00348ac96f5f2214c4a2f39dc0458
Sol <emailsolah@gmail.com>
2024-10-02 23:48:07 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from 'next/head';
 import { DarkModeProvider } from "@/hooks/DarkModeContext";
 import { LanguageProvider } from "@/hooks/LanguageContext";
 // import { SupabaseProvider } from "@/hooks/SupabaseContext";
@@ -8,6 +9,11 @@ import { LanguageProvider } from "@/hooks/LanguageContext";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
+      <Head>
+        <title>{`HireSolah | Your Website Expert`}</title>
+        {/* <!-- Prevent zoom-in at input on mobile --> */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </Head>
       <DarkModeProvider>
         <LanguageProvider>
           {/* <SupabaseProvider> */}
